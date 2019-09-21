d43b14e0d231e41db0f2c711babc328cb870f235
imamfrf <imamfrf@gmail.com>
2019-09-21 15:49:57 +0700

    final

--- app/src/main/java/com/imamfrf/roomcrud/db/NoteRoomDatabase.kt
+++ app/src/main/java/com/imamfrf/roomcrud/db/NoteRoomDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.imamfrf.roomcrud.model.Note
 
-
+//Database annotation to specify the entities and set version
 @Database(entities = [Note::class], version = 1, exportSchema = false)
 abstract class NoteRoomDatabase : RoomDatabase() {
 
@@ -22,8 +22,8 @@ abstract class NoteRoomDatabase : RoomDatabase() {
                     NoteRoomDatabase::class.java,
                     "note_db"
                 )
-                    .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries() //allows Room to executing task in main thread
+                    .fallbackToDestructiveMigration() //allows Room to recreate table if no migrations found
                     .build()
 
                 INSTANCE = instance
--- app/src/main/java/com/imamfrf/roomcrud/model/Note.kt
+++ app/src/main/java/com/imamfrf/roomcrud/model/Note.kt
@@ -6,10 +6,14 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+//Entity annotation to specify the table's name
 @Entity(tableName = "notes")
+//Parcelable annotation to make parcelable object
 @Parcelize
 data class Note(
-    @PrimaryKey(autoGenerate = true) var id: Int = 0,
+    //PrimaryKey annotation to declare primary key
+    //ColumnInfo annotation to specify the column's name
+    @PrimaryKey(autoGenerate = true)@ColumnInfo(name = "id") var id: Int = 0,
     @ColumnInfo(name = "title") var title: String = "",
     @ColumnInfo(name = "body") var body: String = ""
 ) : Parcelable {
